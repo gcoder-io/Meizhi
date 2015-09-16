a1a17eb339ae2cffa934354a4513a5c4a2c7b7f5
lizhongwen <lizhongwen@zaozuo.com.cn>
2015-09-16 18:08:21 +0800

    add library SwiftyJSON

---
+++ Meizhi/business/main/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Meizhi
+//
+//  Created by snowleft on 15/9/16.
+//  Copyright (c) 2015年 ancode. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        println("MainViewController=====")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Podfile
+++ Podfile
@@ -1,13 +1,21 @@
 # install/update请执行命令：pod install --verbose --no-repo-update
 
+# SnapKit 代码编写AutoLayout
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 use_frameworks!
 
 pod 'SnapKit', '~> 0.12.0'
 
+# Alamofire网络库
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 use_frameworks!
 
 pod 'Alamofire', '~> 1.3.1'
+
+# json解析
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'SwiftyJSON', '~> 2.2.1'
