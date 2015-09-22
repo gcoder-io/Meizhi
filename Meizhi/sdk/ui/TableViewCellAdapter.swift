0332001b092a53790aed207c907546c88ef76200
lizhongwen <lizhongwen@zaozuo.com.cn>
2015-09-22 18:48:01 +0800

    modify structure

---
+++ Meizhi/sdk/ui/TableViewCellAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCellAdapter.swift
+//  Meizhi
+//
+//  Created by snowleft on 15/9/22.
+//  Copyright (c) 2015年 ancode. All rights reserved.
+//
+
+import Foundation
---
+++ Meizhi/sdk/ui/TableViewCellHandler.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCellHandler.swift
+//  Meizhi
+//
+//  Created by snowleft on 15/9/22.
+//  Copyright (c) 2015年 ancode. All rights reserved.
+//
+
+import Foundation
